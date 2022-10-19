1b3ad5470244dedb9e21144df3837e5d30ab0558
swapnilpatil2612 <snpatil403@gmail.com>
2022-10-19 14:28:31 +0530

    ArrayList program is added

---
+++ ArrayListDemo/src/ArrayList.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+
+		
+		List<String> list =Arrays.asList("swapnil","mahi","aarav");
+		
+		System.out.println(list);
+		
+		
+	}
+
+}
